b08f5f59ed4e2daa5ac10cae6fa515d8aa2831a7
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2021-02-28 14:02:04 +0100

    done with adding cases

--- app/tests/test_schedule.py
+++ app/tests/test_schedule.py
@@ -4,7 +4,10 @@ import numpy as np
 from app.models import Schedule
 from app.services import ScheduleService, TableService
 
-schedule_service = ScheduleService()
+case = "case2"
+schedule_service = ScheduleService(forced_days_file=case + '/forced_day_off.csv',
+                                   qualified_route_file=case + '/qualified_route.csv',
+                                   pref_days_file=case + '/pref_day_off.csv', )
 schedule = schedule_service.create_schedule()
 np_schedule = schedule.get_schedule()
 
@@ -32,7 +35,7 @@ def test_unique_drivers_in_day(day_index):
 @pytest.mark.parametrize("day_index", [i for i in range(schedule_service.get_no_days())])
 def test_forced_days_off(day_index):
     """Test every shift for every route of the day has unique driver"""
-    forced_day_ser = TableService('forced_day_off.csv')
+    forced_day_ser = schedule_service.forced_days_ser
     day_drivers = schedule.get_day_drivers(day_index)
     off_drivers = forced_day_ser.get_drivers_for_index(day_index)
     sub = list(set(day_drivers) - set(off_drivers))
@@ -42,7 +45,7 @@ def test_forced_days_off(day_index):
 @pytest.mark.parametrize("route_index", [i for i in range(schedule_service.get_no_routes())])
 def test_route_off(route_index):
     """Test every shift for every route of the day has unique driver"""
-    route_driver_ser = TableService('qualified_route.csv')
+    route_driver_ser = schedule_service.qual_route_ser
     route_drivers = schedule.get_route_drivers(route_index)
     qual_route_drivers = route_driver_ser.get_drivers_for_index(route_index)
     sub = list(set(route_drivers) - set(qual_route_drivers))
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from app.services import ScheduleService, TableService
 import numpy as np
 
-schedule_service = ScheduleService()
-schedule_service.update_driver_used_score(1)
-indexes, driver = schedule_service.get_available_drivers_for_route(1, 1)
-
+schedule_service = ScheduleService(
+    forced_days_file='case2/forced_day_off.csv',
+    qualified_route_file='case2/qualified_route.csv',
+    pref_days_file='case2/pref_day_off.csv',
+)
 schedule = schedule_service.create_schedule()
-print(schedule_service.drivers_scores)
+
+# self.raw_data = np.genfromtxt(self.base_dir + file, delimiter=',', skip_header=True)
+# start_id = self.raw_data.shape[0] + 1
+# for row in self.raw_data:
+#     new_row = row
+#     new_row[0] = start_id
+#     np.append(self.raw_data, np.array([row]), axis=0)
+#     start_id += 1
+# schedule_service.update_driver_used_score(1)
+# indexes, driver = schedule_service.get_available_drivers_for_route(1, 1)
+
+# schedule = schedule_service.create_schedule()
 print(schedule.get_schedule())
 ditc_dri = schedule.get_drivers_dict()
 c = 0
 for driver in ditc_dri:
     if "shift2" in ditc_dri[driver]["shifts"] and ditc_dri[driver]["shifts"]["shift2"] > 4:
-        print(driver, ditc_dri[driver]["shifts"]["shift2"] )
+        print(driver, ditc_dri[driver]["shifts"]["shift2"])
         c += 1
 print("############ " + str(c))
-# print(schedule.get_drivers_dict())
 #
 # route_driver_ser = TableService('qualified_route.csv')
 # route_drivers = schedule.get_day_drivers(13)
